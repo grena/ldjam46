26a02bbe4784d621ea67931a5119b217c92c2722
grena <hello@grena.fr>
2020-04-19 17:32:35 +0200

    inspector

--- src/gameobjects/Inspector.ts
+++ src/gameobjects/Inspector.ts
@@ -1,5 +1,6 @@
 import Graphics = Phaser.GameObjects.Graphics;
 import MainScene from "../scene/MainScene";
+import Sprite = Phaser.GameObjects.Sprite;
 
 const Y = 330;
 const X_ARRIVAL = 235;
@@ -9,7 +10,7 @@ const TIME_BEFORE_PHOTO = 2000;
 
 export default class Inspector {
   scene: MainScene;
-  graphics: Graphics;
+  sprite: Sprite;
   photoTime: number;
   hasTookPhoto: boolean;
 
@@ -18,15 +19,31 @@ export default class Inspector {
   }
 
   create() {
-    this.graphics = this.scene.add.graphics({
-      x: 20,
-      y: 20,
-      fillStyle: {
-        color: 0x00ff00,
-        alpha: 1
-      }
+    this.sprite = new Sprite(this.scene, -50, -50, 'inspector');
+    this.sprite.setScale(2, 2);
+
+    this.scene.add.existing(this.sprite);
+    this.runAnimation();
+  }
+
+  private runAnimation() {
+    this.sprite.anims.animationManager.create({
+      key: 'RUN',
+      frames: this.sprite.anims.animationManager.generateFrameNumbers('inspector', { start: 0, end: 5 }),
+      frameRate: 5,
+      repeat: -1
     });
-    this.graphics.fillRect(0, 0, 30, 40);
+    this.sprite.anims.animationManager.create({
+      key: 'PHOTO',
+      frames: this.sprite.anims.animationManager.generateFrameNumbers('inspector', { start: 6, end: 6 }),
+      frameRate: 5,
+      repeat: -1
+    });
+    this.sprite.anims.play('RUN');
+  }
+
+  private stopAnimation() {
+    this.sprite.anims.stop();
   }
 
   prepareVenue(remainingDuration: number) {
@@ -39,26 +56,32 @@ export default class Inspector {
     if (!this.photoTime) {
       return;
     }
+
     const now = this.scene.time.now;
     const timeToArrive = this.photoTime - TIME_BEFORE_PHOTO;
     const timeToGo = this.photoTime + TIME_AFTER_PHOTO;
     let x = X_ARRIVAL;
     if (now > timeToArrive && now < timeToGo) {
+      this.sprite.anims.play('PHOTO');
+      this.stopAnimation();
       if (now > this.photoTime && !this.hasTookPhoto) {
         this.tookPhoto();
       }
     } else if (now < timeToArrive) {
+      if(!this.sprite.anims.isPlaying) this.sprite.anims.play('RUN');
       const remaining = timeToArrive - now;
       x = X_ARRIVAL + remaining / SPEED;
     } else if (now > timeToGo) {
+      if(!this.sprite.anims.isPlaying) this.sprite.anims.play('RUN');
       const remaining = timeToGo - now;
       x = X_ARRIVAL + remaining / SPEED;
     }
 
-    this.graphics.setPosition(x, Y);
+    this.sprite.setPosition(x, Y);
   }
 
   private tookPhoto() {
+    this.sprite.anims.play('RUN');
     this.hasTookPhoto = true;
     this.scene.tookPhoto();
     this.scene.sound.play('photo');
